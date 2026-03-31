e1fc3f227e53c8d121128a6ef43f12c8fbfd2857
Márcio <familiaconfac@gmail.com>
2026-03-31 15:52:54 -0300

    Fix app bootstrap after deploy

--- superintendencia-ebd/src/main.jsx
+++ superintendencia-ebd/src/main.jsx
@@ -2,21 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .getRegistrations()
-      .then(async (registrations) => {
-        await Promise.all(registrations.map((registration) => registration.unregister()))
-        if ('caches' in window) {
-          const cacheKeys = await window.caches.keys()
-          await Promise.all(cacheKeys.map((key) => window.caches.delete(key)))
-        }
-      })
-      .catch((err) => console.warn('[SW] Limpeza falhou:', err))
-  })
-}
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
